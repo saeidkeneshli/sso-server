c4e1bb9cb36d668c2b3b48b844329fad4f696a13
s.keneshli <S.Keneshli@sadad.co.ir>
2026-07-07 17:10:41 +0330

    fix: add JPA Repository

--- src/main/java/io/github/saeidkeneshli/server/repository/UsersRepository.java
+++ src/main/java/io/github/saeidkeneshli/server/repository/UsersRepository.java
@@ -1,15 +1,13 @@
 package io.github.saeidkeneshli.server.repository;
 
 import io.github.saeidkeneshli.server.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository {
-    void save(User user);
-
-    User findById(Long id);
+public interface UsersRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 }
--- src/main/java/io/github/saeidkeneshli/server/service/UserServiceImpl.java
+++ src/main/java/io/github/saeidkeneshli/server/service/UserServiceImpl.java
@@ -3,7 +3,7 @@ package io.github.saeidkeneshli.server.service;
 import io.github.saeidkeneshli.server.dto.register.RegisterRequest;
 import io.github.saeidkeneshli.server.dto.register.RegisterResponse;
 import io.github.saeidkeneshli.server.model.User;
-import io.github.saeidkeneshli.server.repository.UserRepository;
+import io.github.saeidkeneshli.server.repository.UsersRepository;
 import io.github.saeidkeneshli.server.util.PasswordUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,24 +20,18 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserDetailsService, UserService {
 
-    private final Map<String, User> userDatabase = new HashMap<>();
-    private final UserRepository userRepository;
-
-
-    public Optional<User> findByUsername(String username) {
-        return Optional.ofNullable(userDatabase.get(username));
-    }
+    private final UsersRepository usersRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = findByUsername(username)
+        User user = usersRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), Collections.emptyList());
     }
 
     @Override
     public RegisterResponse registerNewUser(RegisterRequest registerRequest) {
-        if (userRepository.findByUsername(registerRequest.getUsername()).isPresent()){
+        if (usersRepository.findByUsername(registerRequest.getUsername()).isPresent()){
             System.out.println("user already registered.");
             return null;
         }
@@ -52,7 +46,7 @@ public class UserServiceImpl implements UserDetailsService, UserService {
                 .address(registerRequest.getAddress().isEmpty() ? "" : registerRequest.getAddress())
                 .build();
 
-        userRepository.save(user);
+        usersRepository.save(user);
         return null;
     }
 }
